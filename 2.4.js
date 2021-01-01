4b586eb9535c743f00f3adf15a8c93375e0c9e5b
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-01-01 23:57:29 +0900

    Add files via upload

---
+++ 2.4.js
@@ -0,0 +1,37 @@
+// #1.
+let message;
+
+message = 'Hello'; // 문자열을 저장
+
+// #2.
+let message;
+message = 'Hello!';
+
+alert(message); // 변수에 저장된 값을 보여줌
+
+// #3.
+let message = 'Hello!'; // 변수를 정의하고 값을 할당
+
+alert(message); // Hello!
+
+// #4. 한 줄에 여러 변수 선언하기
+let user = 'John', age = 25, message = 'Hello';
+
+// #5. 한 줄에 한 개의 변수 작성하기
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+// #6-1. 다양한 변수 선언 방식
+let user = 'John',
+  age = 25,
+  message = 'Hello';
+
+// #6-2.
+let user = 'John'
+  , age = 25
+  , message = 'Hello';
+
+
+
+
