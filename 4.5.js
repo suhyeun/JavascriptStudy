acab4f552df35e69d4afe45511ed2a7e13a2fcd2
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-01-09 23:57:22 +0900

    Add files via upload

--- 4.5.js
+++ 4.5.js
@@ -28,4 +28,46 @@ new User(); // function User { ... }
 // 반면 new와 함께 호출한 경우엔 new.target은 함수 자체를 반환
 
 
+// #3. 생성자와 return문
+// 생성자에 return문을 만들기 
+// 객체를 리턴하여 this대신 객체가 반환됨
+function BigUser() {
 
+    this.name = "John";
+  
+    return { name: "Godzilla" };  // <-- this가 아닌 새로운 객체를 반환함
+}
+  
+alert( new BigUser().name );  // Godzilla
+
+// return문을 무시
+function SmallUser() {
+
+    this.name = "John";
+  
+    return; // <-- this를 반환함
+}
+  
+alert( new SmallUser().name );  // John
+
+// 인수가 없는 생성자 함수는 괄호를 생략해 호출할 수 있음
+
+// #4. 생성자 내 메서드
+function User(name) {
+    this.name = name;
+  
+    this.sayHi = function() {
+      alert( "My name is: " + this.name );
+    };
+}
+  
+let john = new User("John");
+  
+john.sayHi(); // My name is: John
+  
+/*
+john = {
+    name: "John",
+    sayHi: function() { ... }
+}
+*/
