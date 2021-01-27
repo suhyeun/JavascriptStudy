9de9f36eb0dc92e79d57c5f5c5f6c92c5d148d57
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-01-27 23:45:50 +0900

    Add files via upload

--- 2.15.js
+++ 2.15.js
@@ -7,3 +7,13 @@ function showMessage() {
   
 showMessage();
 showMessage();
+
+// 지역 변수
+function showMessage() {
+  let message = "안녕하세요!"; // 지역 변수
+
+  alert( message );
+}
+
+showMessage(); // 안녕하세요!
+alert( message ); // message는 함수 내 지역 변수이기 때문에 에러 발생
