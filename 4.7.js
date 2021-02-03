d59df763edac193ecd8b479447b397730c314559
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-02-03 23:57:40 +0900

    Add files via upload

--- 4.7.js
+++ 4.7.js
@@ -8,4 +8,10 @@ let id = Symbol("id");
 
 // 심볼은 문자형으로 자동 형 변환되지 않음
 
-
+// 숨김 프로퍼티
+let user = { // 서드파티 코드에서 가져온 객체
+    name: "John"
+};
+let id = Symbol("id");
+user[id] = 1; 
+alert( user[id] ); // 심볼을 키로 사용해 데이터에 접근할 수 있습니다.
