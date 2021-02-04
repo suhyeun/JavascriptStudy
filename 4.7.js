0f75b58a1dabdc1acfa8197d2d450f676b0bb1c6
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-02-04 23:56:48 +0900

    Add files via upload

--- 4.7.js
+++ 4.7.js
@@ -15,3 +15,26 @@ let user = { // 서드파티 코드에서 가져온 객체
 let id = Symbol("id");
 user[id] = 1; 
 alert( user[id] ); // 심볼을 키로 사용해 데이터에 접근할 수 있습니다.
+
+
+// Symbols in a literal
+let id = Symbol("id");
+
+let user = {
+  name: "John",
+  [id]: 123 // "id": 123은 안됨
+};
+
+
+// 심볼은 for ... in.. 에서 배제됨
+let id = Symbol("id");
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123
+};
+
+for (let key in user) alert(key); // name과 age만 출력되고, 심볼은 출력되지 않습니다.
+
+// 심볼로 직접 접근하면 잘 작동합니다.
+alert( "직접 접근한 값: " + user[id] );
