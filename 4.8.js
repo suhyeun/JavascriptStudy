e644fd83511462ad8a7538e5716f673eb6c2907e
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-02-09 23:47:12 +0900

    Add files via upload

--- 4.8.js
+++ 4.8.js
@@ -7,3 +7,17 @@ alert(obj);
 
 // 객체를 프로퍼티 키로 사용하고 있음
 anotherObj[obj] = 123;
+
+
+// "number"
+// 수학적 연산을 적용하려 할 때(객체 - 숫자형 변환), hint는 number가 됨.
+// 명시적 형 변환
+let num = Number(obj);
+
+// (이항 덧셈 연산을 제외한) 수학 연산
+let n = +obj; // 단항 덧셈 연산
+let delta = date1 - date2;
+
+// 크고 작음 비교하기
+let greater = user1 > user2;
+
