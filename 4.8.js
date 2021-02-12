f5f09e3cf695eaca8069fe10a295a01dd85792f4
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-02-12 23:34:13 +0900

    Add files via upload

--- 4.8.js
+++ 4.8.js
@@ -37,3 +37,18 @@ obj[Symbol.toPrimitive] = function(hint) {
     // 반드시 원시값을 반환해야 합니다.
     // hint는 "string", "number", "default" 중 하나가 될 수 있습니다.
 };
+
+let user = {
+    name: "John",
+    money: 1000,
+  
+    [Symbol.toPrimitive](hint) {
+      alert(`hint: ${hint}`);
+      return hint == "string" ? `{name: "${this.name}"}` : this.money;
+    }
+};
+  
+// 데모:
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
