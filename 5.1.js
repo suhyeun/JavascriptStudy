380e5ceef16ebaf4bcf5ab65b483e43e0deb5d5b
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-02-17 23:20:13 +0900

    Add files via upload

--- 5.1.js
+++ 5.1.js
@@ -9,4 +9,34 @@ let john = {
     }
   };
   
-  john.sayHi(); // 친구야 반갑다!
+john.sayHi(); // 친구야 반갑다!
+
+// 원시값을 객체처럼 사용하기
+// 인수로 받은 문자열의 모든 글자를 대문자로 바꿔주는 메서드 str.toUpperCase
+let str = "Hello";
+
+alert( str.toUpperCase() ); // HELLO
+
+// 원하는 자리에서 소수점 아래 숫자를 반올림 할 수 있는 toFixed(n)
+let n = 1.23456;
+
+alert( n.toFixed(2) ); // 1.23
+
+// String / Number / Boolean 을 생성자로 쓰는 것을 추천하지 않음
+// 몇몇 상황에서 혼동을 불러일으킴
+// ex1)
+alert( typeof 0 ); // "number"
+
+alert( typeof new Number(0) ); // "object"!
+// 얼럿 창은 무조건 열림
+
+// ex2)
+let zero = new Number(0);
+
+if (zero) { // 변수 zero는 객체이므로, 조건문이 참이 됩니다.
+  alert( "그런데 여러분은 zero가 참이라는 것에 동의하시나요!?!" );
+}
+// new를 붙이지 않고 사용 하는 것은 괜찮음
+
+// ex3)
+let num = Number("123"); // 문자열을 숫자로 바꿔줌
