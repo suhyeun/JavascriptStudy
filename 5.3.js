03687b490e490d4c924cdf1c907a1b23dab4f0aa
suhyeun <56942468+suhyeun@users.noreply.github.com>
2021-04-11 23:46:14 +0900

    Add files via upload

---
+++ 5.3.js
@@ -0,0 +1,12 @@
+// 따옴표
+let single = '작은따옴표';
+let double = "큰따옴표";
+let backticks = `백틱`; // 표현식을 ${…}로 감싸고 이를 백틱으로 감싼 문자열 중간에 넣어주면 해당 표현식을 문자열 중간에 쉽게 삽입
+// 템플릿 리터럴(template literal)
+
+// ex)
+function sum(a, b) {
+  return a + b;
+}
+
+alert(`1 + 2 = ${sum(1, 2)}.`); // 1 + 2 = 3.
